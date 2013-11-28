e05b0b09ad9ec517ca2b9f9c8d455c26c19cf6e2
narkisr <narkisr@gmail.com>
2013-11-28 04:39:53 +0200

    moving to daily cron job

--- manifests/schedule.pp
+++ manifests/schedule.pp
@@ -1,36 +1,11 @@
-# Schedule a backup job for daily/monthly periods.
-define backup::schedule(
-  $hour='',
-  $monthday=undef,
-  $type='duply',
-  $trickle=undef
-) {
+# Schedule a daily backup job. 
+define backup::schedule {
 
-  $log = "/var/log/duply_${name}.log"
-  $tee = "sudo tee ${log} 2>&1"
-  $lock = "flock -n /tmp/${$name}"
-  $base_run = "duply ${name} backup_verify_purge --force | ${tee}"
-
-  if($trickle!=undef){
-    $run = "${lock} trickle -u ${trickle} -s ${base_run}"
-    if !defined(Package['trickle']) {
-      package{'trickle':
-        ensure  => present
-      }
-    }
-  } else {
-    $run = "${lock} ${base_run}"
+  file { "/etc/cron.daily/duply-$name":
+    ensure=> file,
+    mode  => '0777',
+    content => template('backup/cron.erb'),
+    owner => root,
+    group => root,
   }
-
-  cron { "${name}_duply_backup_cron":
-    ensure   => present,
-    command  =>  $type ? {
-      'duply' => $run
-    },
-    user     => 'root',
-    hour     => $hour,
-    monthday => $monthday,
-    require  => File["/etc/duply/${name}/conf"],
-  }
-
 }
