2d90af663e234d72a69030495f66d4869141b973
narkisr <narkisr@gmail.com>
2014-01-02 02:06:43 +0200

    headless dropbox

--- manifests/dropbox.pp
+++ manifests/dropbox.pp
@@ -1,21 +1,67 @@
 # Dropbox installation and managment
-class backup::dropbox {
+# In order to link account in headless mode:
+# sudo su -l dropbox -s /bin/bash
+# umask 0027
+# /usr/local/dropbox/.dropbox-dist/dropboxd
+class backup::dropbox($headless=false) {
 
-  $os_lowercase = downcase($::operatingsystem)
+  if($headless) {
+    archive { 'dropbox-deamon':
+      ensure        => present,
+      url           => 'https://www.dropbox.com/download?plat=lnx.x86_64',
+      digest_string => 'fb324583a0945b96d327f206350f1f32',
+      target        => '/usr/local/dropbox',
+    }
 
-  apt::source { 'dropbox':
-    location          => "http://linux.dropbox.com/${os_lowercase}",
-    release           => $::lsbdistcodename,
-    repos             => 'main',
-    include_src       => false,
-  }
+    group{'dropbox':
+      ensure  => present
+    } ->
 
-  apt::key { 'dropbox':
-    key        => '5044912E',
-    key_server => 'pgp.mit.edu',
-  } ->
+    user{'dropbox':
+      ensure     => present,
+      comment    => 'dropbox',
+      groups     => 'dropbox',
+      managehome => true,
+      home       => '/home/dropbox',
+      shell      => '/bin/false',
+      system     => true
+    } ->
 
-  package{'dropbox':
-    ensure  => present
+    file{'/home/dropbox/Dropbox':
+      ensure => directory,
+      owner  => 'dropbox',
+      group  => 'dropbox'
+    } ->
+
+    file { '/etc/init/dropbox.conf':
+      ensure=> file,
+      mode  => '0700',
+      source=> 'puppet:///modules/backup/dropbox.conf',
+      owner => root,
+      group => root,
+    }
+
+    file{'/etc/init.d/dropbox':
+      ensure => link,
+      target => '/etc/init/dropbox.conf'
+    }
+  } else {
+    $os_lowercase = downcase($::operatingsystem)
+
+    apt::source { 'dropbox':
+      location          => "http://linux.dropbox.com/${os_lowercase}",
+      release           => $::lsbdistcodename,
+      repos             => 'main',
+      include_src       => false,
+    } ->
+
+    apt::key { 'dropbox':
+      key        => '5044912E',
+      key_server => 'pgp.mit.edu',
+    } ->
+
+    package{'dropbox':
+      ensure  => present
+    }
   }
 }
