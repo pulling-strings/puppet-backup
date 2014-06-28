9b299a71ef26ff78381aa13b544f88c6a18421da
narkisr <narkisr@gmail.com>
2014-06-28 20:28:47 +0300

    adding freebsd support

--- manifests/syncthing.pp
+++ manifests/syncthing.pp
@@ -1,31 +1,47 @@
 # setting up https://github.com/calmh/syncthing
 class backup::syncthing($repos=[]) {
-  $version = 'v0.8.16'
-  $release = "syncthing-linux-amd64-${version}"
+  case $::operatingsystem {
+      'FreeBSD': {
+        $os = 'freebsd'
+        $sum ='6d8fd2751e6eecad68dcc6db68b44c86'
+        $target = '/usr/local'
+        Package {
+          provider => pkgng
+        }
+      }
+      'Ubuntu': {
+        $os = 'linux'
+        $sum = 'a04002d8abce148e504147550b81df60'
+        $target = '/opt'
+        file { '/etc/init/syncthing.conf':
+          ensure  => file,
+          mode    => 'u+x',
+          content => template('backup/syncthing.erb'),
+          owner   => root,
+          group   => root,
+        } ->
+
+        file{'/etc/init.d/syncthing':
+          ensure => link,
+          target => '/etc/init/syncthing',
+          mode   => 'a+x',
+        } -> Service['syncthing']
+      }
+      default: {fail('no macthing os found')}
+  }
+
+  $version = 'v0.8.17'
+  $release = "syncthing-${os}-amd64-${version}"
   $url = "https://github.com/calmh/syncthing/releases/download/${version}/${release}.tar.gz"
 
   archive {"syncthing-${version}":
     ensure        => present,
     url           => $url,
-    digest_string => 'f6549b777f0df3a664e128eab0b25169',
+    digest_string => $sum,
     src_target    => '/usr/src',
-    target        => '/opt/',
+    target        => $target,
     extension     => 'tar.gz',
   }
-
-  file { '/etc/init/syncthing.conf':
-    ensure  => file,
-    mode    => 'u+x',
-    content => template('backup/syncthing.erb'),
-    owner   => root,
-    group   => root,
-  } ->
-
-  file{'/etc/init.d/syncthing':
-    ensure => link,
-    target => '/etc/init/syncthing',
-    mode   => 'a+x',
-  } ->
 
   service{'syncthing':
     ensure    => running,
@@ -33,12 +49,12 @@ class backup::syncthing($repos=[]) {
     hasstatus => true,
   }
 
-  file{["/opt/syncthing-${version}/.config","/opt/syncthing-${version}/.config/syncthing"]:
-    ensure => directory,
+  file{["${target}/syncthing-${version}/.config","${target}/syncthing-${version}/.config/syncthing"]:
+    ensure  => directory,
     require => Archive["syncthing-${version}"]
   } ->
 
-  file { "/opt/syncthing-${version}/.config/syncthing/config.xml":
+  file { "${target}/syncthing-${version}/.config/syncthing/config.xml":
     ensure  => file,
     mode    => 'u+rw',
     content => template('backup/config.xml.erb'),
