ddfc21c2aa37e052436dbd4c1279eff0ab84f4f5
narkisr <narkisr@gmail.com>
2014-08-30 16:53:06 +0300

    migrating to 0.9.9 and adding user/pass support'

--- manifests/syncthing.pp
+++ manifests/syncthing.pp
@@ -1,11 +1,16 @@
 # setting up https://github.com/calmh/syncthing
-class backup::syncthing($repos={}, $nodes={}) {
+class backup::syncthing(
+  $repos={},
+  $nodes={},
+  $user = 'vagrant',
+  $password = '$2a$10$2V8KEPD0vkIqtxeZSikS3OljbZFs1mHf5XujeYBEg197Ht7ua5XA2'
+) {
 
-  $version = 'v0.8.17'
+  $version = 'v0.9.9'
 
   case $::operatingsystem {
       'FreeBSD': {
-        $sum ='6d8fd2751e6eecad68dcc6db68b44c86'
+        $sum ='cdd03491123a9d342500b5a1d2158f15'
         $target = '/usr/local'
         $release = "syncthing-freebsd-amd64-${version}"
         class{'backup::syncthing::freebsd':
@@ -17,7 +22,7 @@ class backup::syncthing($repos={}, $nodes={}) {
       'Ubuntu': {
         $os = 'linux'
         $release = "syncthing-linux-amd64-${version}"
-        $sum = 'a04002d8abce148e504147550b81df60'
+        $sum = '58f276046946af7d19462c23e1a87f1d'
         $target = '/opt'
         $group = 'root'
         include backup::syncthing::ubuntu
@@ -26,7 +31,7 @@ class backup::syncthing($repos={}, $nodes={}) {
       default: {fail('no macthing os found')}
   }
 
-  $url = "https://github.com/calmh/syncthing/releases/download/${version}/${release}.tar.gz"
+  $url = "https://github.com/syncthing/syncthing/releases/download/${version}/${release}.tar.gz"
 
   archive {'syncthing':
     ensure        => present,
