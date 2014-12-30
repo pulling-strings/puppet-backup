d96090f66eb92360e61dbe740224c215091eab2e
ronen <narkisr@gmail.com>
2014-12-31 01:01:08 +0200

    attic fuse support

--- manifests/attic.pp
+++ manifests/attic.pp
@@ -1,5 +1,5 @@
 # Setting up https://attic-backup.org/index.html
-class backup::attic($owner='root') {
+class backup::attic($owner='root',$fuse=false) {
   ensure_resource('class', 'apt', {})
 
   ensure_resource('package', 'build-essential', {ensure => present})
@@ -14,7 +14,23 @@ class backup::attic($owner='root') {
   exec{'installing attic':
     command => 'pip3 install attic',
     user    => 'root',
-    path    => ['/usr/bin','/bin']
+    path    => ['/usr/bin','/bin'],
+    unless  => '/usr/bin/pip3 list | grep Attic'
+  }
+
+  if($fuse == true){
+    package{['python3-dev', 'libattr1-dev', 'libfuse-dev',
+              'fuse', 'pkg-config']:
+      ensure  => present
+    } ->
+
+    exec{'installing llfuse':
+      command => 'pip3 install llfuse',
+      user    => 'root',
+      path    => ['/usr/bin','/bin'],
+      unless  => '/usr/bin/pip3 list | grep llfuse',
+      require => Package['python3.2', 'python3.2-dev', 'python3-pip', 'libacl1-dev']
+    }
   }
 
 }
