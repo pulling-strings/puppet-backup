7a58237d3272dd8bcb277b569bc63485a2979969
ronen <narkisr@gmail.com>
2015-09-16 01:50:27 +0300

    adding rclone'

---
+++ manifests/rclone.pp
@@ -0,0 +1,19 @@
+# setting up http://rclone.org/
+class backup::rclone(
+  $version = 'v1.20'
+) {
+  $url = "http://downloads.rclone.org/rclone-${version}-linux-amd64.zip"
+
+  ensure_packages(['unzip'])
+
+  archive {'rclone':
+    ensure        => present,
+    url           => $url,
+    digest_string => 'a73a76bad165b35309e7dc71a17ee543',
+    src_target    => '/usr/src',
+    target        => '/opt/',
+    extension     => 'zip',
+    require       => Package['unzip']
+  }
+
+}
