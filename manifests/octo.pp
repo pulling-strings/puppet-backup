be28794254b27f339960a7af3d62ff37e0682325
ronen <narkisr@gmail.com>
2017-02-19 22:06:22 +1100

    adding octo

--- manifests/octo.pp
+++ manifests/octo.pp
@@ -1,11 +1,15 @@
 # Octo github backup
 class backup::octo {
+
+  include barbecue
+  include jdk
+
   package{'octo':
     ensure  => present,
     require => [Apt::Source['barbecue'], Class['apt::update']]
   }
 
-  file { "/etc/cron.daily/octo":
+  file {'/etc/cron.daily/octo':
     ensure  => file,
     mode    => '0777',
     content => template('backup/duply_cron.erb'),
@@ -13,6 +17,4 @@ class backup::octo {
     group   => root,
   }
 
-
- 
 }
