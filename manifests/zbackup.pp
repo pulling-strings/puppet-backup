5c0c471f10009274c912080bc23360b4cf700976
ronen <narkisr@gmail.com>
2017-02-26 15:04:10 +1100

    using package

--- manifests/zbackup.pp
+++ manifests/zbackup.pp
@@ -1,45 +1,13 @@
 # Setting up zbackup from source
 class backup::zbackup {
 
-  include downloadfile
+  include barbecue
 
-  archive {'zbackup':
-    ensure           => present,
-    url              => 'https://github.com/zbackup/zbackup/archive/1.4.4.tar.gz',
-    digest_string    => '0753ca5d61533f951d6ebb6f087efa0b' ,
-    src_target       => '/usr/src',
-    target           => '/opt/',
-    strip_components => 1
+  package{'zbackup':
+    ensure  => present,
+    require => [Apt::Source['barbecue'], Class['apt::update']]
   }
 
-  ensure_packages(['build-essential'])
-
-  package{['cmake', 'protobuf-compiler', 'libprotobuf-dev', 'liblzo2-dev',
-            'liblzma-dev','libssl-dev']:
-    ensure  => present
-  } ->
-
-  exec{'cmake zbackup':
-    command => 'cmake .',
-    user    => 'root',
-    path    => ['/usr/bin','/bin',],
-    cwd     => '/opt/zbackup',
-    unless  => 'test -f /usr/bin/local/zbackup',
-    require => Package['build-essential']
-  } ->
-
-  exec{'make zbackup':
-    command => 'make',
-    user    => 'root',
-    path    => ['/usr/bin','/bin',],
-    cwd     => '/opt/zbackup',
-    unless  => 'test -f /usr/bin/local/zbackup',
-  } ->
-
-  file{'/usr/local/bin/zbackup':
-    ensure => link,
-    target => '/opt/zbackup/zbackup'
-  }
 
 
 }
