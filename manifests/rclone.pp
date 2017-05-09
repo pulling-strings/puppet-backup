b378b58da9dfa27fd54e17fcb5df9aff67a2b110
ronen <narkisr@gmail.com>
2017-05-09 20:52:53 +1000

    using package

--- manifests/rclone.pp
+++ manifests/rclone.pp
@@ -1,36 +1,11 @@
 # setting up http://rclone.org/
-class backup::rclone(
-  $version = 'v1.35'
-) {
+class backup::rclone {
+  include barbecue
 
-  $arch = $::architecture ? {
-    'amd64'  => 'amd64',
-    'armv6l' => 'arm'
+  package{'rclone':
+    ensure  => present,
+    require => [Apt::Source['barbecue'], Class['apt::update']]
   }
 
-  $sum = $::architecture ? {
-    'amd64'  => '398d20b3c849e5c20d0a9c1db8d25bfd',
-    'armv6l' => '3f358b653dc04b8304d0a5cb5a6e444b'
-  }
-
-  
-  $url = "https://github.com/ncw/rclone/releases/download/${version}/rclone-${version}-linux-${arch}.zip"
-
-  ensure_packages(['unzip'])
-
-  archive {'rclone':
-    ensure        => present,
-    url           => $url,
-    digest_string => $sum,
-    src_target    => '/usr/src',
-    target        => '/opt/',
-    extension     => 'zip',
-    require       => Package['unzip']
-  } ->
-
-  file{'/usr/bin/rclone':
-    ensure => link,
-    target => "/opt/rclone/rclone-${version}-linux-${arch}/rclone"
-  }
 
 }
