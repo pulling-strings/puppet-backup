08e01c8e1bb19fcfb4ada6626a789fa2e543ca4f
ronen <narkisr@gmail.com>
2018-02-27 00:17:11 +1100

    comments

--- manifests/ulimit.pp
+++ manifests/ulimit.pp
@@ -1,10 +1,12 @@
 # Increasing max file handle limits
 class backup::ulimit {
+  # Dropbox
   file_line { 'max-watches':
     path => '/etc/sysctl.conf',
     line => 'fs.inotify.max_user_watches = 200000'
   }
 
+  # Dropbox
   file_line { 'file-max':
     path => '/etc/sysctl.conf',
     line => 'fs.file-max = 800000'
