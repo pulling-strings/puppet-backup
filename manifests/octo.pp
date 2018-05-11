85a7f1b6c1d2b27220b299519672d72948e7f374
ronen <narkisr@gmail.com>
2018-05-12 00:21:54 +1000

    moving to openjdk

--- manifests/octo.pp
+++ manifests/octo.pp
@@ -2,9 +2,12 @@
 class backup::octo {
 
   include barbecue
-  include jdk
 
-  package{'octo':
+  package{'openjdk-8-jre':
+    ensure  => present
+  }
+
+  -> package{'octo':
     ensure  => present,
     require => [Apt::Source['barbecue'], Class['apt::update']]
   }
